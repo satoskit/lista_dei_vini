2edff97072ede8cc03efce4a22dc1b6e03ca1d70
Satomi <satoskit@users.noreply.github.com>
2020-03-30 12:37:49 +0200

    added a floating button and deleted 'add' button in the header

--- lista-dei-vini-client/screens/List.jsx
+++ lista-dei-vini-client/screens/List.jsx
@@ -1,17 +1,28 @@
 import React, { useLayoutEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { black } from 'color-name';
 
 export default function List({navigation}) {
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <Button onPress={() => navigation.navigate('EditList')} title="Add" />
-            )
-        })
-    });
+    // useLayoutEffect(() => {
+    //     navigation.setOptions({
+    //         headerRight: () => (
+    //             <Button onPress={() => navigation.navigate('EditList')} title="Add" />
+    //         )
+    //     })
+    // });
     return (
         <View style={styles.container}>
-            <Text>There will be a list of wines!</Text>
+            <View>
+                <Text>There will be a list of wines!</Text>
+            </View>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('EditList')} 
+                style={styles.addButton}
+            >
+                <Icon name="plus-circle" size={30} color="#990000"/>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -21,5 +32,13 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    addButton: {
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 15,
+        right: 12,
+        width: 60,
+        height: 60,
     }
 });
