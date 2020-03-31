e244cf09e8a96677925a10adaed901be68c9b818
Satomi <satoskit@users.noreply.github.com>
2020-03-31 14:56:34 +0200

    imported files via Spring initializr

---
+++ src/main/java/com/sato/listadeiviniapp/ListaDeiViniAppApplication.java
@@ -0,0 +1,13 @@
+package com.sato.listadeiviniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeiViniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeiViniAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sato/listadeiviniapp/ListaDeiViniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sato.listadeiviniapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeiViniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
