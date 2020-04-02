d886f8946c58d751238624b2fce5665b4d830e1b
Satomi <satoskit@users.noreply.github.com>
2020-04-02 16:05:56 +0200

    added annotation to enable JPA auditing

--- lista-dei-vini-server/src/main/java/com/sato/listadeiviniapp/ListaDeiViniAppApplication.java
+++ lista-dei-vini-server/src/main/java/com/sato/listadeiviniapp/ListaDeiViniAppApplication.java
@@ -2,8 +2,10 @@ package com.sato.listadeiviniapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ListaDeiViniAppApplication {
 
 	public static void main(String[] args) {
