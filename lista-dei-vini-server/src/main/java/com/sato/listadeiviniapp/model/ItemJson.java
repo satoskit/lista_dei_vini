d14e6de47c8aa520c252b660f51812f43e289935
Satomi <satoskit@users.noreply.github.com>
2020-04-08 14:41:56 +0200

    removed @JsonIgnoreProperties

--- lista-dei-vini-server/src/main/java/com/sato/listadeiviniapp/model/ItemJson.java
+++ lista-dei-vini-server/src/main/java/com/sato/listadeiviniapp/model/ItemJson.java
@@ -5,7 +5,6 @@ import java.sql.Timestamp;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties({ "grade", "year"})
 public class ItemJson {
 	
 	@JsonProperty("id")
