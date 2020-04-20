3d79a821f33ec5549fa686574cfe02c0d59d7563
Satomi <satoskit@users.noreply.github.com>
2020-04-20 12:48:07 +0200

    modified styling

--- lista-dei-vini-client/screens/CheckImage.jsx
+++ lista-dei-vini-client/screens/CheckImage.jsx
@@ -1,26 +1,51 @@
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet } from 'react-native';
+import { Dimensions, Image, StyleSheet, View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 
 export default function CheckImage({navigation, route}) {
-    const [ isLoading, setLoading ] = useState(route.params);
+    // const [ isLoading, setLoading ] = useState(route.params);
     const { imageBase64Long } = route.params;
+    const [ imageSize, setImageSize ] = useState({width: Dimensions.get('window').width, height: null });
+
+    // const windowWidth = Dimensions.get('window').width;
+    // const windowHeight = Dimensions.get('window').height;
     
-    // useEffect(() => {
-        
-    // }, [])
-    console.log(imageBase64Long)
+    useEffect(() => {
+        Image.getSize(imageBase64Long, (width, height) => {
+            if (imageSize.width && !imageSize.height) {
+                setImageSize({
+                    width: imageSize.width,
+                    height: height * (imageSize.width / width)
+                });
+            } else if (!imageSize.width && imageSize.height) {
+                setImageSize({
+                    width: width * (imageSize.height / height),
+                    height: imageSize.height
+                });
+            } else {
+                setImageSize({ width: width, height: height });
+            }
+        })
+    }, [])
+    
     return(
-        <Image style={styles.showImage}
-        source={{uri: imageBase64Long}}
-        />
+        <View style={styles.container}>
+            <Image style={/*styles.showImage,*/ {
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: imageSize.height, 
+                width: imageSize.width,
+                resizeMode: 'contain',}}
+            source={{uri: imageBase64Long}}
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    showImage: {
-        // flex: 1,
-        height: 300,
-        width: 300,
-        resizeMode: 'contain',
+    container: {
+        flex: 1,
+        backgroundColor: 'grey'
     },
 })
