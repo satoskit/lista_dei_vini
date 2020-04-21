0ed95efa4435b9a9d70fd56a37c763c6166792c9
Satomi <satoskit@users.noreply.github.com>
2020-04-21 11:49:46 +0200

    small changes

--- lista-dei-vini-client/components/BottleImage.jsx
+++ lista-dei-vini-client/components/BottleImage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function BottleImage({source, big}) {
     // to test
@@ -11,7 +12,7 @@ export default function BottleImage({source, big}) {
     } else if(source.startsWith('/9g')) {
         sourceUri = 'data:image/jpg;base64,' + source
     } else {
-        return <Text>Error</Text>
+        return <View style={big ? stlyes.noimageBig : stlyes.noimageSmall}><Text>No Image</Text><Icon name="image" size={20} /></View>
     }
 
     return (
@@ -26,12 +27,26 @@ export default function BottleImage({source, big}) {
 const stlyes = StyleSheet.create({
     small: {
         resizeMode: 'center',
-        width: 50, 
-        height: 50,
+        width: 60, 
+        height: 60,
     },
     big: { // TODO: set size 
         resizeMode: 'center',
         width: 100, 
         height: 100,
-    }
+    },
+    noimageSmall: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 60, 
+        height: 60,
+    },
+    noimageBig: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100, 
+        height: 100,
+    },
 })
